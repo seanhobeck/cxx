452da0543f4b09050898c482b58a089936b35b81
seanhobeck <121409961+seanhobeck@users.noreply.github.com>
2024-09-05 14:03:44 -0400

    initial commit

--- format.h
+++ format.h
@@ -1,26 +1,26 @@
 /*
- * This file is part of the CXX distribution (https://github.com/seanhobeck/cxx).
- * Copyright (c) 2015 Liviu Ionescu.
+ *		@brief: This header is an extension of the libstdc++ project, specifically adding c++ compatible format functions.
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, version 3.
+ *		This file is part of CXX (https://github.com/seanhobeck/cxx).
+ *		Copyright (c) 2024 Sean Hobeck.
  *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * General Public License for more details.
+ *		This program is free software: you can redistribute it and/or modify
+ *		it under the terms of the GNU General Public License as published by
+ *		the Free Software Foundation, either version 3 of the License, or
+ *		(at your option) any later version.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *		This program is distributed in the hope that it will be useful, but
+ *		WITHOUT ANY WARRANTY; without even the implied warranty of
+ *		MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ *		General Public License for more details.
  *
+ *		You should have received a copy of the GNU General Public License
+ *		along with this program. If not, see <http://www.gnu.org/licenses/>.
  *
+ *		@date    05 / 09 / 24
  *
- *      @author  Sean Hobeck
- *       @date 2024-08-29
- *
- *
- **/
+ */
+
 #ifndef CXX_FORMAT_H
 #define CXX_FORMAT_H
 
@@ -33,19 +33,14 @@
 /// @uses: std::optional, std::nullopt
 #include <optional>
 
-namespace std {
+namespace std
+_GLIBCXX_VISIBILITY(default) {
     /// @fn: formats a string (with a specified length).
-    /// @tparam: pargs_t a template for packed arguments (no va_args).
-    /// @param: _format the string that is going to be formatted.
-    /// @param: _n the length of characters you want to format.
-    /// @param: ..._args virtual packed arguments to format.
-    template<typename ... pargs_t>
+    template<typename... pargs_t>
     _GLIBCXX_NODISCARD
-    static std::string
-    vnformat(const std::string& _format, std::size_t _n, pargs_t... _args) 
-    {
-        if(auto sz = (std::size_t) std::snprintf( nullptr, 0, _format.c_str(), _args...) + 1; sz > 0 && _n != 0)
-        {
+    inline std::string
+    vnformat(const std::string &_format, std::size_t _n, pargs_t... _args) {
+        if (auto sz = (std::size_t) std::snprintf(nullptr, 0, _format.c_str(), _args...) + 1; sz > 0 && _n != 0) {
             std::unique_ptr<char[]> buf(new char[_n]);
             std::snprintf(buf.get(), _n, _format.c_str(), _args...);
             return std::string(buf.get(), buf.get() + _n - 1);
@@ -54,15 +49,11 @@ namespace std {
     }
 
     /// @fn: formats a string.
-    /// @tparam: pargs_t a template for packed arguments (no va_args).
-    /// @param: _format the string that is going to be formatted.
-    /// @param: ..._args virtual packed arguments to format.
-    template<typename ... pargs_t>
+    template<typename... pargs_t>
     _GLIBCXX_NODISCARD
-    static std::string
+    inline std::string
     vformat(const std::string _format, pargs_t... _args) {
-        if(auto sz = (std::size_t) std::snprintf( nullptr, 0, _format.c_str(), _args...) + 1; sz > 0)
-        {
+        if (auto sz = (std::size_t) std::snprintf(nullptr, 0, _format.c_str(), _args...) + 1; sz > 0) {
             std::unique_ptr<char[]> buf(new char[sz]);
             std::snprintf(buf.get(), sz, _format.c_str(), _args...);
             return std::string(buf.get(), buf.get() + sz - 1);
@@ -71,27 +62,20 @@ namespace std {
     }
 
     /// @fn: formats a string (with std::optional).
-    template<typename ... pargs_t>
+    template<typename... pargs_t>
     _GLIBCXX_NODISCARD
-    static std::optional<std::string>
-    voformat(const std::string& _format, pargs_t... _args) 
-    {
+    inline std::optional<std::string>
+    voformat(const std::string &_format, pargs_t... _args) {
         auto s = vformat(_format, _args...);
         return s.length() == 0 ? std::make_optional(s) : std::nullopt;
     }
 
     /// @fn: formats a string (with a specified length).
-    /// @tparam: pargs_t a template for packed arguments (no va_args).
-    /// @param: _format the string that is going to be formatted.
-    /// @param: _n the length of characters you want to format.
-    /// @param: ..._args virtual packed arguments to format.
-    template<typename ... pargs_t>
+    template<typename... pargs_t>
     _GLIBCXX_NODISCARD
-    static std::wstring
-    wnformat(const std::wstring& _format, std::size_t _n, pargs_t... _args) 
-    {
-        if(auto sz = (std::size_t) std::swprintf( nullptr, 0, _format.c_str(), _args...) + 1; sz > 0 && _n != 0)
-        {
+    inline std::wstring
+    wnformat(const std::wstring &_format, std::size_t _n, pargs_t... _args) {
+        if (auto sz = (std::size_t) std::swprintf(nullptr, 0, _format.c_str(), _args...) + 1; sz > 0 && _n != 0) {
             std::unique_ptr<wchar_t[]> buf(new wchar_t[_n]);
             std::swprintf(buf.get(), _n, _format.c_str(), _args...);
             return std::wstring(buf.get(), buf.get() + _n - 1);
@@ -100,16 +84,11 @@ namespace std {
     }
 
     /// @fn: formats a non-unicode string.
-    /// @tparam: pargs_t a template for packed arguments (no va_args).
-    /// @param: _format the string that is going to be formatted.
-    /// @param: ..._args virtual packed arguments to format.
-    template<typename ... pargs_t>
+    template<typename... pargs_t>
     _GLIBCXX_NODISCARD
-    static std::wstring
-    wformat(const std::wstring _format, pargs_t... _args) 
-    {
-        if(auto sz = (std::size_t) std::swprintf( nullptr, 0, _format.c_str(), _args...) + 1; sz > 0)
-        {
+    inline std::wstring
+    wformat(const std::wstring _format, pargs_t... _args) {
+        if (auto sz = (std::size_t) std::swprintf(nullptr, 0, _format.c_str(), _args...) + 1; sz > 0) {
             std::unique_ptr<wchar_t[]> buf(new wchar_t[sz]);
             std::swprintf(buf.get(), sz, _format.c_str(), _args...);
             return std::wstring(buf.get(), buf.get() + sz - 1);
@@ -118,11 +97,10 @@ namespace std {
     }
 
     /// @fn: formats a wstring (with std::optional).
-    template<typename ... pargs_t>
+    template<typename... pargs_t>
     _GLIBCXX_NODISCARD
-    static std::optional<std::wstring>
-    woformat(const std::wstring& _format, pargs_t... _args) 
-    {
+    inline std::optional<std::wstring>
+    woformat(const std::wstring &_format, pargs_t... _args) {
         auto s = wformat(_format, _args...);
         return s.length() == 0 ? std::make_optional(s) : std::nullopt;
     }
--- print.h
+++ print.h
@@ -1,26 +1,26 @@
 /*
- * This file is part of the CXX distribution (https://github.com/seanhobeck/cxx).
- * Copyright (c) 2015 Liviu Ionescu.
+ *		@brief: This header is an extension of the libstdc++ project, specifically adding the much needed print header.
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, version 3.
+ *		This file is part of CXX (https://github.com/seanhobeck/cxx).
+ *		Copyright (c) 2024 Sean Hobeck.
  *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * General Public License for more details.
+ *		This program is free software: you can redistribute it and/or modify
+ *		it under the terms of the GNU General Public License as published by
+ *		the Free Software Foundation, either version 3 of the License, or
+ *		(at your option) any later version.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *		This program is distributed in the hope that it will be useful, but
+ *		WITHOUT ANY WARRANTY; without even the implied warranty of
+ *		MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ *		General Public License for more details.
  *
+ *		You should have received a copy of the GNU General Public License
+ *		along with this program. If not, see <http://www.gnu.org/licenses/>.
  *
+ *		@date    05 / 09 / 24
  *
- *      @author  Sean Hobeck
- *       @date 2024-08-29
- *
- *
- **/
+ */
+
 #ifndef CXX_PRINT_H
 #define CXX_PRINT_H
 
@@ -33,23 +33,23 @@
 /// @uses: std::vformat, std::wformat
 #include "format.h"
 
-namespace std {
+namespace std
+_GLIBCXX_VISIBILITY(default) {
     /// @fn: prints out to stdout, with formatted args.
     /// @tparam: ...pargs_t packed args (same as virtual arguments).
     /// @param: _format the string to be formatted to.
     /// @param: _args format parameters to format the string and print to stdout.
-    template<typename ... pargs_t>
-    static void 
-    print(const std::string& _format, pargs_t... _args) noexcept 
-    { 
-        std::cout << vformat(_format, _args...); 
+    template<typename... pargs_t>
+    inline void
+    print(const std::string &_format, pargs_t... _args) noexcept {
+        std::cout << vformat(_format, _args...);
     }
+
     /// @fn: prints out to a file stream, with formatted args.
-    template<typename ... pargs_t>
-    static void 
-    print(const std::ostream& _fs, const std::string& _format, pargs_t... _args) noexcept 
-    { 
-        _fs << vformat(_format, _args...); 
+    template<typename... pargs_t>
+    inline void
+    print(const std::ostream &_fs, const std::string &_format, pargs_t... _args) noexcept {
+        _fs << vformat(_format, _args...);
     }
 
 
@@ -57,96 +57,92 @@ namespace std {
     /// @tparam: ...pargs_t packed args (same as virtual arguments).
     /// @param: _format the string to be formatted to.
     /// @param: _args format parameters to format the string and print a line to stdout.
-    template<typename ... pargs_t>
-    static void 
-    println(const std::string& _format, pargs_t... _args) noexcept 
-    { 
-        std::cout << vformat(_format, _args...) << std::endl; 
+    template<typename... pargs_t>
+    inline void
+    println(const std::string &_format, pargs_t... _args) noexcept {
+        std::cout << vformat(_format, _args...) << std::endl;
     }
+
     /// @fn: prints a line out to a file stream, with formatted args.
-    template<typename ... pargs_t>
-    static void 
-    println(const std::ostream& _fs, const std::string& _format, pargs_t... _args) noexcept 
-    { 
+    template<typename... pargs_t>
+    inline void
+    println(const std::ostream &_fs, const std::string &_format, pargs_t... _args) noexcept {
         _fs << vformat(_format, _args...) << std::endl;
     }
 
 
     /// @fn: writes out to a file pointer, with formatted args (unicode).
     /// @tparam: ...pargs_t packed args (same as virtual arguments).
-    /// @param: _fp 
+    /// @param: _fp
     /// @param: _format the string to be formatted to.
     /// @param: _args format parameters to format the string and print a line to the file pointer.
-    template<typename ... pargs_t>
-    static void 
-    vprint(FILE* _fp, const std::string& _format, pargs_t... _args) noexcept 
-    { 
+    template<typename... pargs_t>
+    inline void
+    vprint(FILE *_fp, const std::string &_format, pargs_t... _args) noexcept {
         if (auto _f = vformat(_format, _args...); _f.length() > 0)
             fwrite(_f.c_str(), _f.length(), 1ul, _fp);
     }
+
     /// @fn: writes out to stdout with formatted args (unicode).
-    template<typename ... pargs_t>
-    static void 
-    vprint(const std::string& _format, pargs_t... _args) noexcept 
-    { 
+    template<typename... pargs_t>
+    inline void
+    vprint(const std::string &_format, pargs_t... _args) noexcept {
         vprint(stdout, _format, _args...);
     }
-    
-    
+
+
     /// @fn: writes out to a file pointer, ending the line, with formatted args (unicode).
     /// @tparam: ...pargs_t packed args (same as virtual arguments).
     /// @param: _fp the file pointer.
     /// @param: _format the string to be formatted to.
     /// @param: _args format parameters to format the string and print a line to the file pointer.
-    template<typename ... pargs_t>
-    static void 
-    vprintln_unicode(FILE* _fp, const std::string& _format, pargs_t... _args) noexcept 
-    { 
+    template<typename... pargs_t>
+    inline void
+    vprintln_unicode(FILE *_fp, const std::string &_format, pargs_t... _args) noexcept {
         if (auto _f = vformat(_format, _args...) + '\n'; _f.length() > 0)
             fprintf(_fp, _f.c_str());
     }
+
     /// @fn: writes out to stdout with formatted args (unicode).
-    template<typename ... pargs_t>
-    static void 
-    vprintln_unicode(const std::string& _format, pargs_t... _args) noexcept 
-    { 
+    template<typename... pargs_t>
+    inline void
+    vprintln_unicode(const std::string &_format, pargs_t... _args) noexcept {
         vprintln_unicode(stdout, _format, _args...);
     }
+
     /// @fn: writes out to std::wostream with formatted args (unicode).
-    template<typename ... pargs_t>
-    static void 
-    vprintln_unicode(std::ostream& _stream, std::wstring& _format, pargs_t... _args) noexcept 
-    { 
+    template<typename... pargs_t>
+    inline void
+    vprintln_unicode(std::ostream &_stream, std::wstring &_format, pargs_t... _args) noexcept {
         if (auto _f = wformat(_format, _args...) + std::endl; _f.length() > 0)
             _stream << _f;
     }
-        
-    
+
+
     /// @fn: writes out to a file pointer, ending the line, with formatted args (non-unicode).
     /// @tparam: ...pargs_t packed args (same as virtual arguments).
-    /// @param: _fp 
+    /// @param: _fp
     /// @param: _format the wide string to be formatted to.
     /// @param: _args format parameters to format the wstring and print a line to the file pointer.
-    template<typename ... pargs_t>
-    static void 
-    vprintln_nonunicode(FILE* _fp, const std::wstring& _format, pargs_t... _args) noexcept 
-    { 
+    template<typename... pargs_t>
+    inline void
+    vprintln_nonunicode(FILE *_fp, const std::wstring &_format, pargs_t... _args) noexcept {
         if (auto _f = wformat(_format, _args...) + L'\n'; _f.length() > 0)
             fwprintf(_fp, _f.c_str());
     }
+
     /// @fn: writes out to stdout with formatted args (non-unicode).
-    template<typename ... pargs_t>
-    static void 
-    vprintln_nonunicode(const std::wstring& _format, pargs_t... _args) noexcept 
-    { 
+    template<typename... pargs_t>
+    inline void
+    vprintln_nonunicode(const std::wstring &_format, pargs_t... _args) noexcept {
         if (auto _f = wformat(_format, _args...) + L'\n'; _f.length() > 0)
             std::wcout << _f;
     }
+
     /// @fn: writes out to std::wostream with formatted args (non-unicode).
-    template<typename ... pargs_t>
-    static void 
-    vprintln_nonunicode(std::wostream& _stream, std::wstring& _format, pargs_t... _args) noexcept 
-    { 
+    template<typename... pargs_t>
+    inline void
+    vprintln_nonunicode(std::wostream &_stream, std::wstring &_format, pargs_t... _args) noexcept {
         if (auto _f = wformat(_format, _args...) + L'\n'; _f.length() > 0)
             _stream << _f;
     }
